0c01c7fb8d5fcb74705b82d85a888dee30461b2b
Konrad Grodek <konrad.grodek@gmail.com>
2024-07-06 19:12:54 +0200

    SPS30 driver initial commit (nothing interesting so far)

---
+++ device/dev_serial_sps30.py
@@ -0,0 +1,45 @@
+# https://sensirion.com/media/documents/8600FF88/64A3B8D6/Sensirion_PM_Sensors_Datasheet_SPS30.pdf
+# https://github.com/Sensirion/embedded-uart-sps/tree/master/sps30-uart
+# all bytes big-endian
+
+import serial
+
+from collections import namedtuple
+
+Command = namedtuple('Command', ['code', 'name', 'kind', 'timeout_ms', 'min_version'])
+CMD_START = Command(code=0x00, name='Start Measurement', kind='Execute', timeout_ms=20, min_version=(1, 0))
+CMD_STOP = Command(code=0x01, name='Stop Measurement', kind='Execute', timeout_ms=20, min_version=(1, 0))
+CMD_MEASURE = Command(code=0x03, name='Read Measured Value', kind='Read', timeout_ms=20, min_version=(1, 0))
+CMD_SLEEP = Command(code=0x10, name='Sleep', kind='Execute', timeout_ms=5, min_version=(2, 0))
+CMD_WAKEUP = Command(code=0x11, name='Wake-up', kind='Execute', timeout_ms=5, min_version=(2, 0))
+CMD_CLEAN = Command(code=0x56, name='Start Fan Cleaning', kind='Execute', timeout_ms=20, min_version=(1, 0))
+CMD_SET_AUTO_CLEAN = Command(code=0x80, name='Read/Write Auto Cleaning Interval', kind='Read / Write', timeout_ms=20,
+                             min_version=(1, 0))
+CMD_INFO = Command(code=0xD0, name='Device Information', kind='Read', timeout_ms=20, min_version=(1, 0))
+CMD_VERSION = Command(code=0xD1, name='Read Version', kind='Read', timeout_ms=20, min_version=(1, 0))
+CMD_STATUS = Command(code=0xD2, name='Read Device Status Register', kind='Read', timeout_ms=20, min_version=(2, 2))
+CMD_RESET = Command(code=0xD3, name='Reset', kind='Execute', timeout_ms=20, min_version=(1, 0))
+
+ERRORS = {
+    0x00: 'No error',
+    0x01: 'Wrong data length for this command (too much or little data)',
+    0x02: 'Unknown command',
+    0x03: 'No access right for command',
+    0x04: 'Illegal command parameter or parameter out of allowed range',
+    0x28: 'Internal function argument out of range',
+    0x43: 'Command not allowed in current state',
+}
+
+
+class MOSIFrame:
+    """
+    Implements Master Out Slave In frame, part of SHDLC protocol.
+    It represents the command sent from master (RPi) to the SPS30 sensor.
+    """
+
+    START = 0x7E
+    ADR = 0x00
+    STOP = START
+
+    def __init__(self, command: int):
+        pass
